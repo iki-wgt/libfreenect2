9aeed2a377978d068ea465bdf31a19d9946fd7aa
Steffen Pfiffner <s.pfiffner@gmail.com>
2014-11-05 10:28:45 +0100

    Added dependencies

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -4,6 +4,6 @@ cd `dirname $0`
 DEPENDS_DIR=`pwd`
 
 # Ubuntu debian dependencies
-sudo apt-get install libusb-dev libturbojpeg opencl-headers
+sudo apt-get install libusb-dev libturbojpeg opencl-headers automake libudev-dev
 
 sh ./install_deps.sh
