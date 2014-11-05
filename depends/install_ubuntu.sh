03f41b1690754e0fa99be141e649ec658e8d81a0
Steffen Pfiffner <s.pfiffner@gmail.com>
2014-11-05 10:28:45 +0100

    Added dependencies
    Conflicts:
    	depends/install_ubuntu.sh

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -3,4 +3,11 @@
 cd `dirname $0`
 DEPENDS_DIR=`pwd`
 
+<<<<<<< HEAD
 sh ./install_deps.sh
+=======
+# Ubuntu debian dependencies
+sudo apt-get install libusb-dev libturbojpeg opencl-headers automake libudev-dev
+
+sh ./install_deps.sh
+>>>>>>> 9aeed2a... Added dependencies
